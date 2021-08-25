4f78fa21920f518f76e1c483a55fcb9c997b6abd
Michael Chamunorwa <chamunorwamichael@gmail.com>
2021-08-25 14:10:57 +0200

    removed bodyparser

--- server/server.js
+++ server/server.js
@@ -1,11 +1,19 @@
-var express = require("express");
+const express = require("express");
+
 var app = express();
+app.use(express.urlencoded({extended: true})); 
+app.use(express.json());
 app.get("/", (req, res, next) => {
     res.json(["Tony","Lisa","Michael","Ginger","Food"]);
    });
 
 app.post('/update', (req, res, next)=>{
+//fetch longitude
 
+//fetch latitude
+//fetch range
+//send to GOOGLE API
+//return result
 });
 app.listen(3000, () => {
  console.log("Server running on port 3000");
