aa6cce68bd442ab2ab0c26a7bbc9aaf6c214249e
Michael Chamunorwa <chamunorwamichael@gmail.com>
2021-08-26 17:44:03 +0200

    added new google maps option

--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const {Client} = require("@googlemaps/google-maps-services-js");
+const {_api_key} = require('./keys.js');
 
+const client = new Client({config:{key: _api_key}});
 var app = express();
 app.use(express.urlencoded({extended: true})); 
 app.use(express.json());
@@ -8,16 +11,33 @@ app.get("/", (req, res, next) => {
     res.json(["Tony","Lisa","Michael","Ginger","Food"]);
    });
 
-app.post("/post", (req, res, next)=>{
+app.post("/location-details", (req, res)=>{
 //fetch longitude
-console.debug(req.body.longitude);
+let longitude = req.body.longitude;
 //fetch latitude
-console.debug(req.body.latitude);
-//fetch range
-console.debug(req.body.range);
+let latitude = req.body.latitude;
+let range = req.body.range;
 //send to GOOGLE API
+client.directions({
+    params: {
+        origin: [{ lat: latitude, lng: longitude }],
+        destination:[{ lat: latitude, lng: longitude }],
+        mode: "Walking",
+      },
+      timeout: 1000, // milliseconds
+}).then((r)=>{
+    console.log(r.data.results[0].directions);
+}
+).catch((e)=>{console.log(e.response.data.error_message);})
 //return result
+ res.send({status: "ok"});
+
 });
 app.listen(3000, () => {
  console.log("Server running on port 3000");
 });
+
+//get directions from GoogleMaps
+let getDirections = (latitude, longitude, range)=>{
+
+}
